83bb54499c87913e62b171a7c86f4b279e4b9363
wilmafernandes1993 <fwilma10@gmail.com>
2025-03-29 09:43:47 +0530

    Completed delete expense

--- src/pages/expense/ExpenseDetails.tsx
+++ src/pages/expense/ExpenseDetails.tsx
@@ -1,23 +1,54 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import CurrencyUtils from "../../Utils/CurrencyUtils";
 import DateUtils from "../../Utils/DateUtils";
 import useExpenseByExpenseId from "../../hooks/useExpenseByExpenseId";
+import ConfirmDialog from "../../components/ConfirmDialog";
+import { useState } from "react";
+import { deleteExpenseByExpenseId } from "../../services/expense-service";
+
 
 
 
 const ExpenseDetails = () => {
     const {expenseId} = useParams<{ expenseId: string }>();
+    const [showDialog, setShowDialog] = useState<boolean>(false);
+    
+    const navigate = useNavigate();
+
     if(!expenseId) {
         return <p className="text-danger">Invalid Expense Id</p>;
     }
-    const {expense, errors, isLoading} = useExpenseByExpenseId(expenseId);
+    const {expense, errors, isLoading, setLoader, setErrors} = useExpenseByExpenseId(expenseId);
+
+    const handleCancel = () => {
+        console.log("Cancel is clicked");
+        setShowDialog(false);
+    }
+
+    const handleConfirm = () => {
+        setLoader(true);
+        deleteExpenseByExpenseId(expenseId)
+        .then((response) => {
+            if(response && response.status === 204) {
+                navigate("/");
+            }
+        }
+        )
+        .catch((error) => setErrors(error.message))
+        .finally(() => {
+            setLoader(false);
+            setShowDialog(false);
+        });
+
+       
+    };
    
     return <div className="container mt-2">
     {isLoading && <p>Loading...</p>}
     {errors && <p className="text-danger">{errors}</p>}
 
     <div className="d-flex flex-row-reverse mb-2">
-        <button className="btn btn-sm btn-danger">Delete</button>
+        <button className="btn btn-sm btn-danger" onClick={() => setShowDialog(true)}>Delete</button>
         <button className="btn btn-sm btn-warning mx-2">Edit</button>
         <Link className="btn btn-sm btn-secondary" to="/">Back</Link>
     </div>
@@ -49,6 +80,7 @@ const ExpenseDetails = () => {
             </table>
         </div>
     </div>
+    <ConfirmDialog title="Confirm Delete" message="Are you sure you want to delete this item?" show={showDialog} onCancel={handleCancel} onConfirm={handleConfirm}/>
 </div>
 }
 
